dee482fdd3b831cff6e0e80cb968f69e1ca2a856
aiswarya309 <aishuzz309@gmail.com>
2023-01-20 12:09:45 +0530

    single and write

--- src/component/Single/single.js
+++ src/component/Single/single.js
@@ -1,11 +1,32 @@
+import { Link } from 'react-router-dom'
+import Menu from '../Menu/menu'
 import './single.css'
 function Single() {
     return(
      <div className='single'> 
         <div className='contents'>
-            <img src='https://w0.peakpx.com/wallpaper/492/530/HD-wallpaper-lemon-on-pink-background-citruses-lemons-pink-background-citrus-background.jpg' alt='image is not uploaded'/>
+            <img className='contentImg' src='https://w0.peakpx.com/wallpaper/492/530/HD-wallpaper-lemon-on-pink-background-citruses-lemons-pink-background-citrus-background.jpg' alt='image is not uploaded'/>
+        <div className='user'>
+            <img className='userImg' src='https://c8.alamy.com/comp/2BJP8E7/lemon-watercolor-pattern-background-fruit-wallpaper-painting-drawing-love-peace-2BJP8E7.jpg' alt='img is not uploaded'/>
+        <div className='info'>
+        <span><b>John</b></span>
+        <p>Posted 2 days ago</p>
         </div>
-        <div className='menu'>m</div>
+        <div className='edit'>
+            <Link to={'/write?edit=2'}>
+            <img src='https://e7.pngegg.com/pngimages/461/1024/png-clipart-computer-icons-editing-edit-icon-cdr-angle-thumbnail.png'/>
+            </Link>
+            <img src='https://cdn-icons-png.flaticon.com/512/1345/1345874.png'/>
+        </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies faucibus purus, et rutrum augue varius vel. Curabitur augue sapien, viverra fermentum facilisis eu, pulvinar quis tellus. Aliquam erat volutpat. Nam ut tristique quam. Sed faucibus ligula quis neque mollis gravida. Ut ac turpis est. Pellentesque in sodales libero, in fermentum.<br/> <br/>Aliquam maximus eget urna eget volutpat. Fusce ultricies ex massa, at ullamcorper nibh suscipit sed. Sed pulvinar eros neque, eget iaculis lacus imperdiet ac. Curabitur scelerisque felis nec nulla pellentesque aliquam. Donec lectus dolor, aliquet id lectus et, dignissim gravida justo. Etiam sit amet semper dui. Class aptent taciti sociosqu ad litora torquent.<br/><br/> Sed id malesuada dui, quis imperdiet tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies faucibus purus, et rutrum augue varius vel. Curabitur augue sapien, viverra fermentum facilisis eu, pulvinar quis tellus. Aliquam erat volutpat. Nam ut tristique quam. Sed faucibus ligula quis neque mollis gravida. Ut ac turpis est. Pellentesque in sodales libero, in fermentum nisl. Aliquam maximus eget urna eget volutpat. Fusce ultricies ex massa, at ullamcorper nibh suscipit sed. Sed pulvinar eros neque, eget iaculis lacus imperdiet ac. Curabitur scelerisque felis nec nulla pellentesque aliquam. Donec lectus dolor, aliquet id lectus et, dignissim gravida justo. Etiam sit amet semper dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed id malesuada dui, quis imperdiet tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+
+        </p>
+        </div>
+        <div className='menu'>
+            <Menu/>
+        </div>
 
      </div>
     ) 
--- src/component/Write/write.js
+++ src/component/Write/write.js
@@ -1,7 +1,58 @@
+import './write.css'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 function Write() {
+    const [value, setValue] = useState('');
     return(
-     <div>
-         <h1>Write</h1>
+     <div className="writeW">
+        <div className="contentWrite">
+            <input type='text' placeholder='Title'/>
+            <div className='editContainer'>
+            <ReactQuill className='editor' theme="snow" value={value} onChange={setValue} />
+            </div>
+        </div>
+        <div className="menuW">
+        <div className="itemW">
+            <h1>Publish</h1>
+            <span><b>Status: </b>Draft</span>
+            <span><b>Visibility: </b>Public</span>
+            <input style={{display:'none'}} type='file' name='' id='file'/>
+            <label htmlFor='file' className='fileW'>Upload Image</label>
+            <div className='buttonW'>
+                <button className='first-child' >Sve as a draft</button>
+                <button className='last-child'>Update</button>
+            </div>
+        </div>
+        <div className="item">
+            <h1>Category</h1>
+            <div className='cat'>
+            <input type='radio' name='cat' value='art' id='art'/>
+            <label htmlFor='art'>Art</label>
+            </div>
+            <div className='cat'>
+            <input type='radio' name='cat' value='science' id='science'/>
+            <label htmlFor='science'>Science</label>
+            </div>
+            <div className='cat'>
+            <input type='radio' name='cat' value='technology' id='technology'/>
+            <label htmlFor='technology'>Technology</label>
+            </div>
+            <div className='cat'>
+            <input type='radio' name='cat' value='cinema' id='cinema'/>
+            <label htmlFor='cinema'>cinema</label>
+            </div>
+            <div className='cat'>
+            <input type='radio' name='cat' value='design' id='design'/>
+            <label htmlFor='design'>Design</label>
+            </div>
+            <div className='cat'>
+            <input type='radio' name='cat' value='food' id='food'/>
+            <label htmlFor='food'>Food</label>
+            </div>
+        </div>
+        </div>
      </div>
     ) 
 }
